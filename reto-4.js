afd50078e2e6e0c6864f6609c1aca33cbcbe30e2
Gabriel Ivan Carrillo <gabrielcarrillo943@gmail.com>
2023-12-07 02:30:27 -0300

    ejercicio 4

---
+++ reto-4.js
@@ -0,0 +1,25 @@
+/*En el taller de Santa 🎅, algunos mensajes navideños han sido escritos de manera peculiar: las letras dentro de los paréntesis deben ser leídas al revés
+
+Santa necesita que estos mensajes estén correctamente formateados. Tu tarea es escribir una función que tome una cadena de texto y revierta los caracteres dentro de cada par de paréntesis, eliminando los paréntesis en el mensaje final.
+
+Eso sí, ten en cuenta que pueden existir paréntesis anidados, por lo que debes invertir los caracteres en el orden correcto. */
+
+function decode(message) {
+  let regex = /\(([^()]+)\)/g;
+  let newMessage;
+  decodificado = message.match(regex);
+
+  if (!decodificado) {
+    newMessage = message;
+  } else {
+    decodificado.forEach((element) => {
+      let arreglado = element.split("").reverse().join("").slice(1, -1);
+      newMessage = message.replace(element, arreglado);
+    });
+    if (newMessage.match(regex)) {
+      newMessage = decode(newMessage);
+    }
+  }
+
+  return newMessage;
+}
